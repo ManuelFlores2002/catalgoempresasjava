93707eb1f34be459b60dfe4f8bd8aa0d5997665a
Manuel Flores <manuelvillalta819@gmail.com>
2023-08-09 08:22:46 -0600

    Clases Empresa y Usuario

--- CatalogoEmpresa.EntidadesDeNegocio/src/catalogoempresa/entidadesdenegocio/Empresa.java
+++ CatalogoEmpresa.EntidadesDeNegocio/src/catalogoempresa/entidadesdenegocio/Empresa.java
@@ -1,5 +1,114 @@
 package catalogoempresa.entidadesdenegocio;
 
+import java.time.LocalDate;
+
 public class Empresa {
+    private int id;
+    private int idRol;
+    private String nombre;
+    private String apellido;
+    private String login;
+    private String password;
+    private byte estatus;
+    private LocalDate fechaRegistro;
+    private String top_aux;
+    private Contacto contacto;
+
+    public Empresa() {
+    }
+
+    public Empresa(int id, int idRol, String nombre, String apellido, String login, String password, byte estatus, LocalDate fechaRegistro, String top_aux, Contacto contacto) {
+        this.id = id;
+        this.idRol = idRol;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.login = login;
+        this.password = password;
+        this.estatus = estatus;
+        this.fechaRegistro = fechaRegistro;
+        this.top_aux = top_aux;
+        this.contacto = contacto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(byte estatus) {
+        this.estatus = estatus;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(String top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+    
     
 }
